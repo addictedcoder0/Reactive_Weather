c397109d2c810cb5132676f50919a5ee0fa8f26d
abhishek <abhishek-2863@abhishek-2863.csez.zohocorpin.com>
2017-04-09 20:37:08 +0530

    modified the server.js in order to run on localhost too

--- server.js
+++ server.js
@@ -5,13 +5,13 @@ var app =  express();
 // Heroku will give a dynamic port to bind
 const PORT = process.env.PORT || 3000 ;
 
-//https issue
+//https is being redirected b/c openweather map api is not working with https , any way we are trying to find another solution
+// on localhost setup this app will break b/c locally header 'x-forwarded-proto' doesn't exist , so change the condition of if block
 // app.use(function(req,res,next){
-//   if(req.headers['x-forwarded-proto'] === 'http'){
-//     // debugger;
-//     next();
-//   }else{
+//   if(req.headers['x-forwarded-proto'] === 'https'){
 //     res.redirect('http://'+ req.hostname + req.url);
+//   }else{
+//     next(); // now it work for even localhost setup too ;)
 //   }
 // });
 
